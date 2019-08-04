107f5520848276ce87388d6198eb4b241bb68622
branezhang <924800501@qq.com>
2019-08-04 22:42:07 +0800

    另存为运行成功，需改善

--- common/desired_caps.py
+++ common/desired_caps.py
@@ -1,18 +1,27 @@
+import xlrd
 from appium import webdriver
 import yaml
 import logging
 import logging.config
 import os
 
-CON_LOG='../config/log.conf'
+CON_LOG = '../config/log.conf'
 logging.config.fileConfig(CON_LOG)
-logging=logging.getLogger()
+logging = logging.getLogger()
+
+
+def get_data(file_path, sheet_name, begin=0, end=5000):  # 获取A1开始第一列的数据
+    file = xlrd.open_workbook(file_path, encoding_override="uft-8")
+    sheet = file.sheet_by_name(sheet_name)
+    # return sheet.col_values(0)[0:2499]
+    return sheet.col_values(0)[begin:end]
+
 
 def appium_desired():
-    with open('../config/yozo_office_caps.yaml','r',encoding='utf-8') as file:
-        data=yaml.load(file,Loader=yaml.FullLoader)
+    with open('../config/yozo_office_caps.yaml', 'r', encoding='utf-8') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
         print(data)
-    desired_caps=data['desired_caps']
+    desired_caps = data['desired_caps']
     # desired_caps['platformName']=data['platformName']
     # desired_caps['platformVersion']=data['platformVersion']
     # desired_caps['deviceName']=data['deviceName']
@@ -31,9 +40,10 @@ def appium_desired():
     # desired_caps['resetKeyboard']=data['resetKeyboard']
 
     logging.info('start app...')
-    driver=webdriver.Remote('http://'+str(data['ip'])+':'+str(data['port'])+'/wd/hub',desired_caps)
+    driver = webdriver.Remote('http://' + str(data['ip']) + ':' + str(data['port']) + '/wd/hub', desired_caps)
     driver.implicitly_wait(8)
     return driver
+
 
 if __name__ == '__main__':
     # print( os.path.dirname(os.path.dirname(__file__)))
@@ -48,4 +58,3 @@ if __name__ == '__main__':
     #
     # app_path=os.path.join(base_dir,'app',data['appname'])
     # print(app_path)
-
