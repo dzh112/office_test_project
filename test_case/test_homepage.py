a75a6179350b442b5b07b3652e49e3aa5f9e670b
Brane <924800501@qq.com>
2019-08-07 17:16:02 +0800

    添加登录删除和标星的测试用例

--- test_case/test_homepage.py
+++ test_case/test_homepage.py
@@ -4,18 +4,51 @@ import logging
 import unittest
 
 from businessView.createView import CreateView
+from businessView.iconView import IconView
+from businessView.loginView import LoginView
 from businessView.searchView import SearchView
 from common.myunit import StartEnd
 from common.tool import rm_file
 
 
 class TestHomePage(StartEnd):
+    csv_file = '../data/account.csv'
+
+    # @unittest.skip('skip test_mark_star')
+    def test_mark_star(self):
+        logging.info('======test_mark_star=====')
+        iv = IconView(self.driver)
+        file_name = iv.mark_star()
+
+        self.assertTrue(iv.check_mark_star(file_name))
+
+    @unittest.skip('skip test_upload')
+    def test_upload(self):
+        logging.info('======test_upload=====')
+        iv = IconView(self.driver)
+        iv.upload()
+
+        self.assertTrue(iv.check_upload())
+
+    @unittest.skip('skip test_login_13915575564')
+    def test_login(self):
+        logging.info('======test_login=====')
+        sv = SearchView(self.driver)
+        sv.user_logo()
+        l = LoginView(self.driver)
+        if l.check_login_status():
+            l.logout_action()
+        data = l.get_csv_data(self.csv_file, 4)
+
+        l.login_action(data[0], data[1])
+        self.assertTrue(l.check_login_status())
 
     @unittest.skip('skip test_user_log')
     def test_user_log(self):
         logging.info('==========test_user_log==========')
         sv = SearchView(self.driver)
         sv.user_logo()
+
         self.assertTrue(sv.check_user_logo())
 
     @unittest.skip('skip test_create')
@@ -24,6 +57,7 @@ class TestHomePage(StartEnd):
         logging.info('==========test_create_search==========')
         cv = CreateView(self.driver)
         cv.create_file('wp')
+
         self.assertTrue(cv.check_create_file())
 
     @unittest.skip('skip test_search')
@@ -32,8 +66,8 @@ class TestHomePage(StartEnd):
         sv = SearchView(self.driver)
         file_name = '00045.docx'
         sv.search_action(file_name)
-        self.assertTrue(sv.check_search_action(file_name))
 
+        self.assertTrue(sv.check_search_action(file_name))
 
 if __name__ == '__main__':
     unittest.main()
