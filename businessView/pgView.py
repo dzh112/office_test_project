c8a5d0f2bc4dda19677c64f34f24015307c93ef0
Brane <924800501@qq.com>
2019-08-30 17:36:17 +0800

    pg切换功能添加

--- businessView/pgView.py
+++ businessView/pgView.py
@@ -9,9 +9,24 @@ from businessView.generalView import GeneralView
 
 class PGView(GeneralView):
 
-
-    def switch_mode(self):  # 切换
+    def switch_mode(self, switch, apply='one'):  # 切换
         logging.info('==========switch_mode==========')
+        # switch = ['无切换', '平滑淡出', '从全黑淡出', '切出', '从全黑切出', '溶解', '向下擦除', '向左擦除', '向右擦除',
+        #           '向上擦除', '扇形展开', '从下抽出', '从左抽出', '从右抽出', '从上抽出', '从左下抽出', '从左上抽出',
+        #           '从右下抽出', '从右上抽出', '盒状收缩', '盒状展开', '1根轮辐', '2根轮辐', '3根轮辐', '4根轮辐', '8根轮辐',
+        #           '上下收缩', '上下展开', '左右收缩', '左右展开', '左下展开', '左上展开', '右下展开', '右上展开', '圆形',
+        #           '菱形', '加号', '新闻快报', '向下推出', '向左推出', '向右推出', '向上推出', '向下插入', '向左插入',
+        #           '向右插入', '向上插入', '向左下插入', '向左上插入', '向右下插入', '向右上插入', '水平百叶窗',
+        #           '垂直百叶窗', '横向棋盘式', '纵向棋盘式', '水平梳理', '垂直梳理', '水平线条', '垂直线条', '随机']
+        ranges = '//android.support.v7.widget.RecyclerView/android.widget.FrameLayout'
+        switch_ele = '//*[@text="%s"]' % switch
+        self.swipe_search2(switch_ele, ranges)
+        self.driver.find_element(By.XPATH, switch_ele).click()
+        if not apply == 'one':
+            for i in range(3):
+                eles = self.driver.find_elements(By.XPATH, ranges)
+                self.swipe_ele1(eles[0],eles[-1])
+            self.driver.find_element(By.ID,'com.yozo.office:id/yozo_ui_pg_option_id_transition_apply_all').click()
 
     def pause_resume_play(self):  # 暂停、回复播放
         logging.info('==========pause_resume==========')
@@ -24,7 +39,7 @@ class PGView(GeneralView):
         logging.info('==========quit_paly==========')
         self.driver.keyevent(4)
 
-    def play_mode(self, mode):  # 播放模式 current,first,autoplay
+    def play_mode(self, mode='autoplay'):  # 播放模式 current,first,autoplay
         logging.info('==========play_mode==========')
         self.driver.find_element(By.ID, 'com.yozo.office:id/yozo_ui_pg_option_id_play_%s' % mode).click()
 
@@ -101,5 +116,14 @@ class PGView(GeneralView):
 
 
 if __name__ == '__main__':
-    for i in range(12):
-        print(i)
+    waylist = ['wx', 'qq', 'ding', 'mail']
+    wps = ['wp', 'ss', 'pg']
+    # ss = list(map(lambda x,y:x+'_'+y,wps,waylist))
+    # ss = list(map(lambda x:x+'_wx',wps))
+    ss = []
+    for i in wps:
+        for j in waylist:
+            ss.append(i+'_'+j)
+    print(ss)
+    # for i in range(12):
+    #     print(i)
